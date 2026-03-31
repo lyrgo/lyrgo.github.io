577d798eca1d041739ed388fbb8ca9cc2f0a26b7
lyrgo <3572691603@qq.com>
2026-03-31 13:09:50 +0800

    upd 二分图

--- docs/.vuepress/algorithm-collection.ts
+++ docs/.vuepress/algorithm-collection.ts
@@ -105,6 +105,7 @@ export const algorithm: ThemeCollectionItem = defineCollection({
                         'dcc',
                     ]
                 },
+                'bipartite',
                 'min-cycle',
             ]
         },
