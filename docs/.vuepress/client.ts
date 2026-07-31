a3222b2b0371c22db622a61e2866af5f5baac075
lyrgo <3572691603@qq.com>
2026-07-31 15:59:51 +0800

    upd alert

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -7,7 +7,7 @@ import { defineClientConfig } from 'vuepress/client'
 // import CustomComponent from './theme/components/Custom.vue'
 
 // @ts-ignore
-import './theme/styles/custom.css'
+import './theme/styles/alert.css'
 
 export default defineClientConfig({
   enhance({ app }) {
