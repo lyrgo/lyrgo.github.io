7f05a5433277b5b8e419e4811255d2339148ec0e
lyrgo <3572691603@qq.com>
2026-08-25 14:21:22 +0800

    upd prufer

--- docs/.vuepress/algorithm-collection.ts
+++ docs/.vuepress/algorithm-collection.ts
@@ -130,6 +130,7 @@ export const algorithm: ThemeCollectionItem = defineCollection({
                         'bigraph-match',
                     ]
                 },
+                'prufer',
             ]
         },
         {
